2cea5a852a26364caa645dfb8a60dc87b68c7bd2
chakrabortyanish <chakrabortyanish981@gmail.com>
2026-06-01 22:05:47 +0530

    fix  routes

--- server/src/routes/uploadCV.routes.js
+++ server/src/routes/uploadCV.routes.js
@@ -1,14 +1,24 @@
 import express from "express";
-import {uploadCV} from "../middlewares/upload_cv.js";
+import { uploadCV } from "../middlewares/upload_cv.js";
 import { protectUser } from "../middlewares/clerkAuth.middleware.js";
 import { uploadCV as uploadCVController } from "../controllers/cvController.js";
 import { updateCV as updateCVController } from "../controllers/cvController.js";
-import { viewCV as  viewCVController} from "../controllers/cvController.js";
+import { viewCV as viewCVController } from "../controllers/cvController.js";
 
 const router = express.Router();
 
-router.post("/upload-cv",protectUser, uploadCV.single("cv"), uploadCVController);
+router.post(
+  "/upload-cv",
+  protectUser,
+  uploadCV.single("cv"),
+  uploadCVController,
+);
 router.get("/my-cv",protectUser, viewCVController);
-router.put("/update-cv",protectUser, uploadCV.single("cv"), updateCVController);
+router.put(
+  "/update-cv",
+  protectUser,
+  uploadCV.single("cv"),
+  updateCVController,
+);
 
 export default router;
